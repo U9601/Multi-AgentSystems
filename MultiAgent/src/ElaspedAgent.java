9dd3334d1e58515a10e907e8a36074a0f18baee9
U9601 <ctjones1965@gmail.com>
2019-09-17 17:24:00 +0100

    fixed elsaped time

--- MultiAgent/src/ElaspedAgent.java
+++ MultiAgent/src/ElaspedAgent.java
@@ -10,12 +10,11 @@ public class ElaspedAgent extends Agent {
 	protected void setup(){
 		loop = new TickerBehaviour(this, 300){
 			protected void onTick(){
-				if(t0 >= 60000){
 					System.out.println(System.currentTimeMillis()-t0 +     ": "+ myAgent.getLocalName());
-				}else{
-					System.out.println("You have reached 1 min");
-					myAgent.doDelete();
-				}
+					if(System.currentTimeMillis()-t0 > 60000 ){
+						System.out.println("You have reached 1 min");
+						myAgent.doDelete();
+					}
 			}
 		};
 		addBehaviour(loop);
